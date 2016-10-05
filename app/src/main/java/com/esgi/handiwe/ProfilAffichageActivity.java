deea36fc4231c992cbf98e20cd99528f4cef2a3b
Asifadam93 <adamsha.asif@gmail.com>
2016-10-05 20:56:30 +0200

    ProfilAffichageActivity test

--- app/src/main/java/com/esgi/handiwe/ProfilAffichageActivity.java
+++ app/src/main/java/com/esgi/handiwe/ProfilAffichageActivity.java
@@ -9,5 +9,9 @@ public class ProfilAffichageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil_affichage);
+
+        /**
+         * Test push github
+         **/
     }
 }
