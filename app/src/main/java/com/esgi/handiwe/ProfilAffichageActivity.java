e996c295956d197fcf3206829a8565d0ab576df6
PicoPark <salome.russier@gmail.com>
2016-10-05 18:24:56 +0200

    commit crea activity
    creation des activity vide

---
+++ app/src/main/java/com/esgi/handiwe/ProfilAffichageActivity.java
@@ -0,0 +1,13 @@
+package com.esgi.handiwe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfilAffichageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_affichage);
+    }
+}
