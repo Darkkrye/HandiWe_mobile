2e3f128f705d199682570448a556fa00fb7b835b
PicoPark <salome.russier@gmail.com>
2016-10-07 11:16:58 +0200

    variable global

--- app/src/main/java/com/esgi/handiwe/Outils/VariableGlobal.java
+++ app/src/main/java/com/esgi/handiwe/Outils/VariableGlobal.java
@@ -2,6 +2,8 @@ package com.esgi.handiwe.Outils;
 
 import android.app.Application;
 
+import com.esgi.handiwe.BLL.ConversationMessageManager;
+import com.esgi.handiwe.BLL.DisponibiliteHeureManager;
 import com.esgi.handiwe.BLL.UtilisateurSportManager;
 
 /**
@@ -11,10 +13,13 @@ import com.esgi.handiwe.BLL.UtilisateurSportManager;
 public class VariableGlobal extends Application {
 
     UtilisateurSportManager utilisateurSportManager = null;
+    ConversationMessageManager conversationMessageManager = null;
+    DisponibiliteHeureManager disponibiliteHeureManager = null;
+
 
     public UtilisateurSportManager getUtilisateurSportManager() {
 
-        if(utilisateurSportManager == null){
+        if (utilisateurSportManager == null) {
             utilisateurSportManager = new UtilisateurSportManager();
         }
 
@@ -24,4 +29,31 @@ public class VariableGlobal extends Application {
     public void setUtilisateurSportManager(UtilisateurSportManager utilisateurSportManager) {
         this.utilisateurSportManager = utilisateurSportManager;
     }
+
+    public ConversationMessageManager getConversationMessageManager() {
+
+        if (conversationMessageManager == null) {
+            conversationMessageManager = new ConversationMessageManager();
+        }
+        return conversationMessageManager;
+    }
+
+    public void setConversationMessageManager(ConversationMessageManager conversationMessageManager) {
+        this.conversationMessageManager = conversationMessageManager;
+    }
+
+     public DisponibiliteHeureManager getDisponibiliteHeureManager() {
+
+        if (disponibiliteHeureManager == null) {
+            disponibiliteHeureManager = new DisponibiliteHeureManager();
+        }
+        return disponibiliteHeureManager;
+    }
+
+    public void setDisponibiliteHeureManager(DisponibiliteHeureManager disponibiliteHeureManager) {
+        this.disponibiliteHeureManager = disponibiliteHeureManager;
+    }
+
+
+
 }
